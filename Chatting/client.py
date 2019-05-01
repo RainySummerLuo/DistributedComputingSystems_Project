8073f47a54143929f66a580d5dc23099db94a23b
Zichen Luo <iluo@163.com>
2019-05-01 15:22:10 +0800

    Update client.py

--- Chatting/client.py
+++ Chatting/client.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import xmlrpc.client
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from gui_client import *
+
 s = xmlrpc.client.ServerProxy('http://localhost:8000')
 
-while 1:
-    sth = input("我说: ")
-    reply = s.say(sth)
-    print("TA说: ", reply)
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    mainwindow = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(mainwindow)
+    mainwindow.show()
+    sys.exit(app.exec_())
+
+# while 1:
+#     sth = input("我说: ")
+#     reply = s.say(sth)
+#     print("TA说: ", reply)
